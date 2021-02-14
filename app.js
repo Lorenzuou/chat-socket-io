9203e28f2d7f1383a2a77243a3f86e41eaefd3ae
Lorenzo Scaramussa <lorenzoscaramussa@hotmail.com>
2021-02-14 16:08:31 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
-
-const express = require('express')
+const Usuario = require("./usuario.js")
+const express = require('express');
+const { futimes } = require('fs');
 const path = require('path');
+
 
 const app = express()
 const port = 3000
@@ -18,17 +20,62 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 let mensagens = [];
-sockets.on('connection', (socket) => {
-  
-  
-  socket.emit('receberHistorico',mensagens); 
-  console.log(socket.id);
+let usuarios = [];
+let online = 0;
 
-  socket.on('enviarMensagem', dado => {
-    // console.log(dado);
-    socket.broadcast.emit('receberMensagem',dado); 
-    mensagens.push(dado);
+
+function verificarUsuario(dado, socket) {
+  let encontrou = 0;
+  for (let i = 0; i < usuarios.length; i++) {
+    if (usuarios[i].nome == dado) {
+      usuarios[i].socketId = socket.id;
+      encontrou = 1;
+      break;
+    }
+  }
+  if (!encontrou) {
+    let novoUsuario = new Usuario(dado, usuarios.length, socket);
+    usuarios.push(novoUsuario);
+
+  } else {
+
+  }
+
+}
+
+sockets.on('connection', (socket) => { // ativavdo quando alguem se conecta
+  
+  online++; // contador de conexões
+  console.log("conectado");
+
+  socket.on('novoUsuario',(dado)=>{
+    verificarUsuario(dado, socket.id);
+    console.log(dado);
+    socket.broadcast.emit('usuarioConectado',dado);
+
   }); 
+  
+  socket.emit('receberHistorico', mensagens);
+
+
+  socket.on('enviarMensagem', (dado) => {
+
+    // console.log(dado);
+    socket.broadcast.emit('receberMensagem', dado);
+    mensagens.push(dado);
+  });
+
+  socket.on('disconnect', () => {
+
+    online = online - 1;
+    console.log(online);
+    let usuarioDesconectado = usuarios.filter((obj) => { return obj.socketId == socket.id});
+    console.log(usuarioDesconectado[0]);
+    if(usuarioDesconectado[0])
+      socket.broadcast.emit('usuarioDesconectado',usuarioDesconectado[0].nome); 
+
+
+  });
 });
 
 app.get('/chat', (req, res) => {
